f58041e916005a715ed631690bec69ce6de79272
nishant patel <nishantpatelbca@gmail.comgit config --global user.name nishantpatel11>
2019-10-20 22:42:56 +0530

    Added Stream API program

--- src/main/java/com/stream/service/impl/StreamImpl.java
+++ src/main/java/com/stream/service/impl/StreamImpl.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import com.stream.model.Fruit;
 import com.stream.model.News;
@@ -21,28 +20,35 @@ public class StreamImpl {
 		List<Fruit> fruits = new ArrayList<Fruit>();
 
 
-		//		
+//		Display fruit  name of low calories sorted in descending order
 		fruits.stream().filter(fruit -> fruit.getCalories() < 100).sorted(new Comparator<Fruit>() {
 			public int compare(Fruit fruit1, Fruit fruit2) {
 				return fruit2.getCalories().compareTo(fruit1.getCalories());
 			}
-		}).forEach(System.out::println);
+		}).forEach(fruit -> System.out.println(fruit.getName()));
 
 
-		//		
-		fruits.stream().sorted(Comparator.comparing(Fruit::getColor)).forEach(System.out::println);
+//		Display color wise list of fruit name
+		fruits.stream().sorted(Comparator.comparing(Fruit::getColor)).forEach(fruit->System.out.println("Color :"+fruit.getColor()+"Fruit Name :"+fruit.getName()));
 
 
-		//		
+//		Display only Red color fruit and ascending order
 		fruits.stream().filter(fruit -> fruit.getColor().equals("RED")).sorted(new Comparator<Fruit>() {
 			public int compare(Fruit fruit1, Fruit fruit2) {
 				return fruit1.getPrice().compareTo(fruit2.getPrice());
 			}
-		}).forEach(System.out::println);
+		}).forEach(fruit -> System.out.println(fruit.getName()));
 
 
-		//		
+		
+		
+		
+//		--------------------------------------------------------------------------------------
+		
+		
 		List<News> newsList = new ArrayList<News>();
+
+//		Budget arrived in user comment all news
 		Optional<News>  newsl = newsList.stream().max(Comparator.comparing(News::getComment));
 		System.out.println(newsl);
 
@@ -54,22 +60,32 @@ public class StreamImpl {
 
 		//		Long numberBudget = newsList.stream().anyMatch(news->)
 
+		
+//		----------------------------------------------------------------------------------------
 
-//
+//		All transaction in 2011 and sort by name
 		List<Transaction> transactionList = new ArrayList<Transaction>();
 		transactionList.stream().filter(transaction -> transaction.getYear() == 2011).sorted(Comparator.comparing(Transaction::getValue)).forEach(System.out::println);
 		
-//		
+//		All uniquines cities
 		transactionList.stream().filter( distinctByKey(transaction -> transaction.getTrader().getCity())).forEach(System.out::println);
 
 		
-//		
+//		All Trader from Pune and sort by name
 		transactionList.stream().filter(transaction -> transaction.getTrader().getCity().equalsIgnoreCase("Pune")).sorted(new Comparator<Transaction>() {
 			public int compare(Transaction t1,Transaction t2) {
 				return t1.getTrader().getName().compareTo(t2.getTrader().getName());
 			}
 		}).forEach(System.out::println);
-	
+
+
+		
+//		Any traders based in Indore
+		transactionList.stream().filter(transaction -> transaction.getTrader().getCity().equalsIgnoreCase("Indore")).forEach(System.out::println);
+		
+		
+		
+		
 	}
 
 	public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor)
